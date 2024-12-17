f0ffc5a807b2be6d3bbc11bc7d1e06362bc588ac
Komuthu <komuthuapsara@gmail.com>
2024-12-17 21:38:55 +0530

    Portfolio webpage, Background gradient done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+
 import ComingSoon from './screens/ComingSoon';
 import BookACall from './screens/BookACall';
 import Error from './screens/Error';
 import AboutUs from './screens/AboutUs';
+import Portfolio from './screens/Portfolio';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -21,6 +23,7 @@ function App() {
         <Route path="*" element={<Navigate to="/404" />} />
         <Route path='/book-a-call' element={<BookACall/>} /> 
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/screens/Portfolio.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import '../css/Portfolio.css';
+import es from '../assets/es-project-image.webp';
+import dob from '../assets/dob-project-image.webp';
+
+const Portfolio = () => {
+
+    return(
+        <div className="portfolio-container">
+            <div className="portfolio-header">
+                <h2>WE ARE STILL YOUNG <br />AND BUILDING</h2>
+                <p>At Sparkhaus, every project is a step in our journey of growth and creativity. While we’re young, our passion and commitment drive us to deliver exceptional results. Explore our portfolio to see how we’ve turned ideas into impactful digital experiences - and know that we’re just getting started.</p>
+            </div>
+            <div className="portfolio-content">
+                <div className="project-card">
+                    <img src={dob} alt="Project Image 1" />
+                    <div className="card-right">
+                        <h3>DOCTOR OF BAT</h3>
+                        <span>UI/UX Design | Development | Content Management</span>
+                        <p>The client wanted to explore the digital landscape and establish a presence online without committing to an online shop just yet. We designed a simple yet functional website that effectively showcased their services and reflected their brand identity. It provided a solid foundation for them to engage with their audience and gain confidence in the web space.</p>
+                    </div>
+                </div>
+
+                <div className="project-card">
+                    <img src={es} alt="Project Image 2" />
+                    <div className="card-right">
+                        <h3>DOCTOR OF BAT</h3>
+                        <span>UI/UX Design | Development | Content Management</span>
+                        <p>The client wanted to explore the digital landscape and establish a presence online without committing to an online shop just yet. We designed a simple yet functional website that effectively showcased their services and reflected their brand identity. It provided a solid foundation for them to engage with their audience and gain confidence in the web space.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Portfolio;
