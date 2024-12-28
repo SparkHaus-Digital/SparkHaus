6623bb1ecaad477a6c0fa82f068a0b859c9f1198
Komuthu <komuthuapsara@gmail.com>
2024-12-28 13:15:33 +0530

    enhancements Navbar, Aboutus, Portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../css/Navbar.css';
 import sparkhausLogo from '../assets/sparkhaus-Horizontal2.png';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const navRef = useRef();
+    const location = useLocation();
 
     const toggleMenu = () => {
         setMenuOpen((prev) => !prev);
@@ -25,8 +26,10 @@ const Navbar = () => {
     }, [menuOpen]);
 
     return (
-        <nav className={`navbar ${menuOpen ? 'navbar-open' : ''}`}
-        ref={navRef}>
+        <nav
+            className={`navbar ${menuOpen ? 'navbar-open' : ''}`}
+            ref={navRef}
+        >
             <Link to="/" className="navbar-logo-container">
                 <img src={sparkhausLogo} className="navbar-logo" alt="logo" />
             </Link>
@@ -41,23 +44,23 @@ const Navbar = () => {
             <ul
                 className={`navbar-links ${menuOpen ? 'active' : ''}`}
             >
-                <li className="navbar-item">
+                <li className={`navbar-item ${location.pathname === '/services' ? 'active' : ''}`}>
                     <Link to="/services" onClick={() => setMenuOpen(false)}>
                         Services
                     </Link>
                 </li>
-                <li className="navbar-item">
+                <li className={`navbar-item ${location.pathname === '/about-us' ? 'active' : ''}`}>
                     <Link to="/about-us" onClick={() => setMenuOpen(false)}>
                         About
                     </Link>
                 </li>
-                <li className="navbar-item">
+                <li className={`navbar-item ${location.pathname === '/portfolio' ? 'active' : ''}`}>
                     <Link to="/portfolio" onClick={() => setMenuOpen(false)}>
                         Portfolio
                     </Link>
                 </li>
                 <li className="mobile-only">
-                    <Link to="/book-a-call" className='navbar-button-mv' onClick={() => setMenuOpen(false)}>
+                    <Link to="/book-a-call" className="navbar-button-mv" onClick={() => setMenuOpen(false)}>
                         Book a Call
                     </Link>
                 </li>
