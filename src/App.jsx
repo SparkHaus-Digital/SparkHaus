108610b682e9cdc086f4dc566d1f27d6029a7ba6
Komuthu <komuthuapsara@gmail.com>
2024-12-31 13:57:17 +0530

    SpeedIns Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 
 import Home from "./screens/Home";
