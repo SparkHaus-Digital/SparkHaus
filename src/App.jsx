ff8224eca7af52760a685b857147c9da2eb90978
Komuthu <komuthuapsara@gmail.com>
2024-12-31 13:46:32 +0530

    Enable vercel insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 import Home from "./screens/Home";
@@ -37,6 +39,8 @@ function App() {
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
       <Footer />
+      <Analytics />
+      <SpeedInsights />
     </Router>
   );
 }
